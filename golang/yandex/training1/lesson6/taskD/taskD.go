20266f3b9f1956e6b6ef59ccc091f5efdb4af0f7
JustNik <justcrazynik@gmail.com>
2023-03-28 20:57:35 +0300

    solve lesson6 task d

---
+++ golang/yandex/training1/lesson6/taskD/taskD.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var n, a, b, w, h uint64
+
+	fmt.Fscan(reader, &n, &a, &b, &w, &h)
+	params := checkParams{
+		a: a,
+		b: b,
+		w: w,
+		h: h,
+		n: n,
+	}
+	ans := rightBinSearch(0, w+h, check, params)
+	fmt.Print(ans)
+}
+
+func rightBinSearch(l uint64, r uint64, check func(uint64, checkParams) bool, params checkParams) uint64 {
+	for l < r {
+		m := (l + r + 1) / 2
+		if check(m, params) {
+			l = m
+		} else {
+			r = m - 1
+		}
+	}
+	return l
+}
+
+func check(m uint64, params checkParams) bool {
+	aNew := params.a + 2*m
+	bNew := params.b + 2*m
+
+	hCnt1 := params.h / aNew
+	wCnt1 := params.w / bNew
+
+	hCnt2 := params.w / aNew
+	wCnt2 := params.h / bNew
+
+	return hCnt1*wCnt1 >= params.n || hCnt2*wCnt2 >= params.n
+}
+
+type checkParams struct {
+	a uint64
+	b uint64
+	w uint64
+	h uint64
+	n uint64
+}
