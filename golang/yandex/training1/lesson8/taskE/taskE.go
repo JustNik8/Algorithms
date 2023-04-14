5ad1c1c255182e75770867edd74570f8e1ca8c75
JustNik <justcrazynik@gmail.com>
2023-04-14 15:15:56 +0300

    solve lesson8 taskE

---
+++ golang/yandex/training1/lesson8/taskE/taskE.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	tree := make([]interface{}, 0)
+
+	var num int
+	fmt.Fscan(reader, &num)
+
+	if num == 0 {
+		fmt.Println(0)
+		return
+	}
+
+	initTree(&tree, num)
+
+	for {
+		fmt.Fscan(reader, &num)
+		if num == 0 {
+			break
+		}
+		add(&tree, num)
+	}
+
+	leftSearch(&tree)
+}
+
+func leftSearch(tree *[]interface{}) {
+	if tree == nil {
+		return
+	}
+
+	left, leftOk := (*tree)[1].([]interface{})
+	right, rightOk := (*tree)[2].([]interface{})
+	if leftOk {
+		leftSearch(&left)
+	}
+	if rightOk {
+		leftSearch(&right)
+	}
+
+	if !leftOk && !rightOk {
+		fmt.Println((*tree)[0])
+	}
+}
+
+func add(tree *[]interface{}, x int) {
+	root := (*tree)[0].(int)
+	if x == root {
+		return
+	} else if x < root {
+		if (*tree)[1] == nil {
+			subTree := make([]interface{}, 0)
+			initTree(&subTree, x)
+			(*tree)[1] = subTree
+		} else {
+			slice := (*tree)[1].([]interface{})
+			add(&slice, x)
+		}
+	} else {
+		if (*tree)[2] == nil {
+			subTree := make([]interface{}, 0)
+			initTree(&subTree, x)
+			(*tree)[2] = subTree
+		} else {
+			slice := (*tree)[2].([]interface{})
+			add(&slice, x)
+		}
+	}
+}
+
+func initTree(tree *[]interface{}, root int) {
+	*tree = append(*tree, root)
+	*tree = append(*tree, nil)
+	*tree = append(*tree, nil)
+}
