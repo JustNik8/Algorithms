4f82983c949d95710bfe68e436b62839893d5901
JustNik <justcrazynik@gmail.com>
2023-04-26 11:33:53 +0300

    solve training2 lesson1 taskB

--- golang/yandex/training2/lesson1/taskB/taskB.go
+++ golang/yandex/training2/lesson1/taskB/taskB.go
@@ -1,5 +1,25 @@
-package maing
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+// https://contest.yandex.ru/contest/28730/problems/B/
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var stationCnt, enter, exit int
 
+	fmt.Fscan(reader, &stationCnt, &enter, &exit)
+
+	straight := math.Abs(float64(enter)-float64(exit)) - 1
+
+	minStation := math.Min(float64(enter), float64(exit))
+	maxStation := math.Max(float64(enter), float64(exit))
+	reverse := float64(stationCnt) - maxStation + minStation - 1.0
+
+	res := int(math.Min(straight, reverse))
+	fmt.Println(res)
 }
