8bb3f2f13d06ed0042ed3f93c67ec1a58604d298
JustNik <justcrazynik@gmail.com>
2023-05-04 23:44:33 +0300

    solve training2 divB lesson4 taskA

--- golang/yandex/training2/divB/lesson4/taskA/taskA.go
+++ golang/yandex/training2/divB/lesson4/taskA/taskA.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
 
+// https://contest.yandex.ru/contest/28970/problems/A/
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(reader, &n)
+
+	parcels := make(map[int64]int64)
+	for i := 0; i < n; i++ {
+		var color, number int64
+		fmt.Fscan(reader, &color, &number)
+		parcels[color] += number
+	}
+
+	sortedKeys := make([]int64, 0)
+	for color := range parcels {
+		sortedKeys = append(sortedKeys, color)
+	}
+
+	sort.Slice(sortedKeys, func(i, j int) bool {
+		return sortedKeys[i] < sortedKeys[j]
+	})
+	for _, color := range sortedKeys {
+		number := parcels[color]
+		fmt.Printf("%d %d\n", color, number)
+	}
 }
