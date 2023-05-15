1c933edd2ee7546eb319a93b57787ae6079a7cea
JustNik <justcrazynik@gmail.com>
2023-05-15 21:25:08 +0300

    solve training3 divb task4

--- golang/yandex/training3/divB/task2/task2.go
+++ golang/yandex/training3/divB/task2/task2.go
@@ -6,7 +6,8 @@ import (
 	"os"
 )
 
-// TL
+type empty struct{}
+
 // https://contest.yandex.ru/contest/45468/problems/2/
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -17,24 +18,45 @@ func main() {
 	var str string
 	fmt.Fscan(reader, &str)
 
-	maxBeauty := 0
-	for i := 0; i < len(str); i++ {
-		pointer := i
-		letter := str[i]
-		changed := 0
-		for changed < k && pointer < len(str) {
-			currentLetter := str[pointer]
-			if letter != currentLetter {
-				changed++
-			}
-			pointer++
-		}
-		for pointer < len(str) && str[pointer] == letter {
-			pointer++
-		}
+	alphabet := make(map[uint8]empty)
 
-		if pointer-i > maxBeauty {
-			maxBeauty = pointer - i
+	// Собираем алфавит из всех букв
+	for i := 0; i < len(str); i++ {
+		alphabet[str[i]] = empty{}
+	}
+
+	// Идея следующая: решаем через 2 указателя. Правым указателем идем и проверяем равна ли текущая буква
+	// букве из алфавита. Если не равна, то вычитаем -1 из changes, Если равна, то просто двигаем правый указатель
+	// правый указатель двигаем до упора строки. Когда буквы для замены закончились, то проверяем длинну красивой строки
+	// Далее левым указателем выкидываем самую левую букву. Если эта буква равна букве из алфавита, то changes не меняется,
+	// иначе прибавляем +1 к changes. Это означает, что если мы выкинули букву не из алфавита, то можем сделать
+	// еще 1 замену
+	maxBeauty := 1
+	for letter := range alphabet {
+		changes := k
+		left := 0
+		right := 0
+
+		for left < len(str) && right < len(str) {
+			for right < len(str) {
+				if changes == 0 && str[right] != letter {
+					break
+				}
+				if str[right] != letter {
+					changes--
+				}
+				right++
+			}
+
+			if right-left > maxBeauty {
+				maxBeauty = right - left
+			}
+			//fmt.Println(string(letter), left, right, right-left)
+
+			if str[left] != letter {
+				changes++
+			}
+			left++
 		}
 	}
 
---
+++ golang/yandex/training3/divB/task4/task4.go
@@ -0,0 +1,6 @@
+package main
+
+// https://contest.yandex.ru/contest/45468/problems/4/
+func main() {
+
+}
