2ab84148e5470879640a57ece42b8b2a4406bd0f
JustNik <justcrazynik@gmail.com>
2023-07-31 17:05:14 +0300

    solve leetcode task110

---
+++ golang/leetcode/easy/110-BalancedBinaryTree/task110.go
@@ -0,0 +1,51 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	balanced := true
+
+	var dfs func(root *TreeNode) int
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+
+		left := dfs(root.Left)
+		right := dfs(root.Right)
+
+		if abs(left-right) > 1 {
+			balanced = false
+		}
+
+		return 1 + max(left, right)
+	}
+
+	dfs(root)
+
+	return balanced
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
