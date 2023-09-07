73668d90b3db248ec157470febf64190b6f4f252
JustNik <justcrazynik@gmail.com>
2023-09-07 10:32:34 +0300

    solve leetcode task124

---
+++ golang/leetcode/hard/124-BinaryTreeMaximumPathSum/task124.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+	maxSum := math.MinInt32
+	pathSum(root, &maxSum)
+
+	return maxSum
+}
+
+func pathSum(node *TreeNode, maxSum *int) int {
+	if node == nil {
+		return 0
+	}
+
+	leftPath := max(0, pathSum(node.Left, maxSum))
+	rightPath := max(0, pathSum(node.Right, maxSum))
+
+	curSum := node.Val + leftPath + rightPath
+
+	*maxSum = max(*maxSum, curSum)
+
+	return node.Val + max(leftPath, rightPath)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
