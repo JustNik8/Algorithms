80fbaef1eb88b1e1e67c458a7878dfac30ba3f06
JustNik <nikita-999999@yandex.ru>
2023-12-05 22:06:27 +0300

    solve leetcode task48

---
+++ golang/leetcode/medium/48-RotateImage/task48.go
@@ -0,0 +1,29 @@
+package main
+
+func rotate(matrix [][]int) {
+	left, right := 0, len(matrix)-1
+
+	for left < right {
+		for i := 0; i < right-left; i++ {
+			top, bottom := left, right
+
+			// save top bottom
+			topLeft := matrix[top][left+i]
+
+			// move bottom left to top left
+			matrix[top][left+i] = matrix[bottom-i][left]
+
+			// move bottom right to bottom left
+			matrix[bottom-i][left] = matrix[bottom][right-i]
+
+			// move top right to bottom right
+			matrix[bottom][right-i] = matrix[top+i][right]
+
+			// move top left to top right
+			matrix[top+i][right] = topLeft
+		}
+
+		left++
+		right--
+	}
+}
