b8e91dea3c6f0601a2baeccb0b5f5a1761ffcae3
JustNik <nikita-999999@yandex.ru>
2023-12-17 16:27:48 +0300

    better solution for leetcode task73

--- golang/leetcode/medium/73-SetMatrixZeroes/task73.go
+++ golang/leetcode/medium/73-SetMatrixZeroes/task73.go
@@ -33,3 +33,50 @@ func setZeroes(matrix [][]int) {
 		}
 	}
 }
+
+// Space - O(1), Time - O(n*m)
+func setZeroes2(matrix [][]int) {
+	n := len(matrix)
+	m := len(matrix[0])
+
+	isFirstRowZero := false
+	isFirstColZero := false
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if matrix[i][j] != 0 {
+				continue
+			}
+
+			if i == 0 {
+				isFirstRowZero = true
+			}
+			if j == 0 {
+				isFirstColZero = true
+			}
+
+			matrix[i][0] = 0
+			matrix[0][j] = 0
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	if isFirstRowZero {
+		for r := 0; r < m; r++ {
+			matrix[0][r] = 0
+		}
+	}
+
+	if isFirstColZero {
+		for c := 0; c < n; c++ {
+			matrix[c][0] = 0
+		}
+	}
+}
