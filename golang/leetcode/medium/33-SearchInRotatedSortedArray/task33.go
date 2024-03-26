58750fd50ac8e2f3f152c81b94a2d9dae584f576
JustNik <nikita-999999@yandex.ru>
2024-03-26 12:43:01 +0300

    add new solution for leetcode task33

--- golang/leetcode/medium/33-SearchInRotatedSortedArray/task33.go
+++ golang/leetcode/medium/33-SearchInRotatedSortedArray/task33.go
@@ -34,3 +34,34 @@ func search(nums []int, target int) int {
 
 	return ans
 }
+
+// one else solution
+func search2(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := (l + r) / 2
+
+		if nums[m] == target {
+			return m
+		}
+
+		if nums[l] <= nums[m] {
+			// Left portion
+			if target < nums[l] || target > nums[m] {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
+		} else {
+			// Right portion
+			if target > nums[r] || target < nums[m] {
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		}
+	}
+
+	return -1
+}
